3fd942b846de16ff95284cc8f52936950699ed4b
devahe <devahe@hotmail.com>
2015-12-06 21:19:41 +0000

    alerts added to submit & contact buttons

---
+++ client/js/buttons.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#submitBtn').click(function(){
+        alert("Your ad has been submitted!");
+    });
+});
+
+$(document).ready(function(){
+    $('#contactBtn').click(function(){
+        alert("We will get back to you as soon as possible!");
+    });
+});
